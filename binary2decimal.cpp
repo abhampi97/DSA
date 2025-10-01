526bfdca45baa8aeb7e27068dc5bbe4ac7450234
Abhay Pratap <superxpoint01@gmail.com>
2025-10-01 22:16:28 +0530

    Add files via upload

---
+++ binary2decimal.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int decimal(int n)
+{
+   int i,d=0,rem,pow=1;
+   for(i=0;n>0;i++)
+   {
+       rem=n%10;
+       n=n/10;
+       d=d+rem*pow;
+       pow=pow*2;
+   }
+   cout<<"Decimal num is : "<<d;
+}
+int main()
+{
+    int n;
+    cout<<"enter binary number :";
+    cin>>n;
+    decimal(n);
+    return 0;
+}
