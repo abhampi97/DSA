8ba43fa30daca5d58ae20049363b1d363339d842
Abhay Pratap <superxpoint01@gmail.com>
2025-10-02 11:47:31 +0530

    Update bin.cpp

--- bin.cpp
+++ bin.cpp
@@ -78,12 +78,12 @@ int bin(int n)
     num=n; 
     }
     int a[20],i=1,c=0;
-  while(num>0)// 1t (12>0) True|2t (6>0) True|3t (3>0) True|4t (1>0) true|5t (0>0) False
+  while(num>0)
   {
-   a[i]=num%2; //1t a[1]=12%2; a[1]=0;|2t a[2]=6%2; a[2]=0;|3t a[3]=3%2;a[3]=1;a[4]=1%2:a[4]=1;
-    num=num/2; //1t num=12/2; num=6;|2t num=6/2; num=3; |3t num=3/2; num=1;|4t num=1/2;num=0;
-    c++; //t1 c=1;|t2 c=2;|3t c=3;|4t c=4;
-    i++;//t1 i=1;|t2 i=2;|3t i=3;|4t i=4;
+   a[i]=num%2; 
+    num=num/2; 
+    c++;
+    i++;
   }
   print(c,a,n);//(4,a{0,0,1,1},-12);
 }
@@ -95,4 +95,5 @@ int main()
     cin>>n;// suppose you enter : -12
     bin(n);//bin(-12);
     return 0;
+
 }
